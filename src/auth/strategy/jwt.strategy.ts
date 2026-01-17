80d530406a2aaf8461e84daa83a80f969786cb54
Kishan Gupta <kishangupta4514@gmail.com>
2026-01-17 23:53:29 +0530

    Implement the guard in the controller

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    const secret = config.get<string>('JWT_SECRET');
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    // You can add more logic here to fetch user from DB if needed
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    if (!user) {
+      return null;
+    }
+
+    const { hash, ...userWithoutHash } = user;
+    return userWithoutHash;
+  }
+}
